c8ac5e9cbc3a14fee40b51e04d5482a4085b5aba
slawomirmatuszak <s.matuszak@gmail.com>
2023-09-05 18:30:14 +0200

    wyczyszczenie skryptu

--- skrypty/pobor_obrobka.R
+++ skrypty/pobor_obrobka.R
@@ -23,8 +23,7 @@ dane_raw <- read.table("./dane/migracje.gz", header = T) |>
   mutate(across(2:18, ~as.numeric(.))) |> 
   #slice(1:100) |> 
   rename(nazwa = 1) |> 
-  separate(col = nazwa, into = c(NA,"obywatelstwo","plec","wiek","kraj"), sep = ",") |> 
-  filter(obywatelstwo == "UA")
+  separate(col = nazwa, into = c(NA,"obywatelstwo","plec","wiek","kraj"), sep = ",")
   
 
 dane <- dane_raw |> 
@@ -32,35 +31,4 @@ dane <- dane_raw |>
   rename(data = name, liczba = value) |> 
   mutate(data = ymd(paste0(str_sub(data, 2,5), str_sub(data, 7,8), "01")))
 
-wiek_plec_pl <- dane |> 
-  filter(kraj %in% c("PL", "DE", "CZ"), 
-         data == max(data), 
-         plec %in% c("M", "F"), 
-         wiek %in% c("Y_LT14", "Y14-17", "Y18-34", "Y35-64", "Y_GE65")) |> 
-  mutate(wiek = factor(wiek, levels = c("Y_LT14", "Y14-17", "Y18-34", "Y35-64", "Y_GE65"))) |> 
-  mutate(liczba = if_else(plec == "M", liczba*-1, liczba))
-
-pop_range <- range(wiek_plec_pl$liczba)
-#pop_range_seq <- seq(pop_range[1], pop_range[2], 5e4)
-pop_range_breaks <- pretty(pop_range, n = 5)
-
-
-ggplot(wiek_plec_pl, aes(wiek, liczba, fill = plec))+
-  geom_col() +
-  scale_y_continuous(breaks  = pop_range_breaks,
-                     labels = abs(pop_range_breaks)/1000) +
-  coord_flip()+
-  facet_wrap(~kraj)+
-  theme_bw()
-
-# proporcje mężczyzn i kobiet
-wiek_plec_pl |> 
-  mutate(liczba = if_else(plec == "M", liczba*-1, liczba)) |> 
-  group_by(kraj, wiek) |> 
-  mutate(prop = round(liczba/sum(liczba),3)) |> 
-ggplot(aes(wiek, prop, fill = plec))+
-  geom_col(position = "fill")+
-  geom_text(aes(label = scales::percent(prop)), position = position_stack(.5))+
-  coord_flip()+
-  facet_wrap(~kraj)+
-  theme_bw()
+save(dane, file = "./dane/dane.Rda")
